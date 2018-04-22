1134f2e5b0eb49d91b7051c34539ffcc0d460944
Raka Wiradinata <rakawiradinata@gmail.com>
2018-04-21 22:25:31 -0400

    add dynamic generatePlayerBoard()

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,18 +1,26 @@
-// Create an empty array for the board
-let board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
+/*
 
-// Function to print board
-let printBoard = board => {
-  console.log('Current Board: ');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
-};
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+DESCRIPTION: MINESWEEPER
+
+- Dynamically create a game board (player guesses) & a bomb board
+(hold bomb locations)
+
+*/
+
+// CONST FUNCTION FOR A BLANK BOARD
+//// 1. Add an empty space to each column per row
+//// 2. Add each row to a larger game board, constructing player board
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  //create empty board
+  let board = [];
+  //for each row, create columns and push ' '
+  for (i = 0; i < numberOfRows; i++) {
+    let row = [];
+    for (j = 0; j < numberOfColumns; j++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
+}
+//console.log(generatePlayerBoard(3, 3));
