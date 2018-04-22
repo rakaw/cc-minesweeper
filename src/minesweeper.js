630119752e3b273f0c1b51b99a8975b870caed2b
Raka Wiradinata <rakawiradinata@gmail.com>
2018-04-21 21:11:59 -0400

    Initial commit

---
+++ src/minesweeper.js
@@ -0,0 +1,18 @@
+// Create an empty array for the board
+let board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
+
+// Function to print board
+let printBoard = board => {
+  console.log('Current Board: ');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+printBoard(board);
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board);
