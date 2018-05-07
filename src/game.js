5426b8231de12fa6ca2d80abdd9370371eb0d2ca
Raka Wiradinata <rakawiradinata@gmail.com>
2018-05-07 18:30:30 -0400

    finishing touches

--- src/game.js
+++ src/game.js
@@ -1,30 +1,32 @@
 // INSTRUCTIONS
-//  To play Minesweeper, we wil create instances of MineSweeperGame
+//  To play Minesweeper, we will create instances of MineSweeperGame
 //    in command line.
 //  For Example:
 //    In the command line, navigate to the lib directory and rune `node`.
-//    Run `.load game .js` to load the contents of this file.
+//    Run `.load game.js` to load the contents of this file.
 //    Then create a Game instance and run commands like so:
-//      let game - new game(3, 3, 3);
+//      let game = new Game(3, 3, 3);
 //      game.playMove(0, 1);
 //      game.playMove(1, 2);
+//        etc.
 //      When done, run `.exit`;
 
 
-import Board from './board.js';
+import { Board } from './board.js';
 
 
 
 /* CREATE GAME SUPERCLASS
 1. Create a template for a minsweeper game
 */
-export class Game {
+class Game {
   constructor (numberOfRows,numberOfColumns,numberOfBombs) {
     this._board = new Board(numberOfRows,numberOfColumns,numberOfBombs);
   }
   // conditions for winning, losing, and continuing play
   playMove (rowIndex,columnIndex) {
     this._board.flipTile(rowIndex,columnIndex);
+
     if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
       console.log('GAME OVER.');
       this._board.print()
