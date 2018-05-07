3c34c25d963a7be9517fb264a33846c9b4d3a894
Raka Wiradinata <rakawiradinata@gmail.com>
2018-05-07 17:39:00 -0400

    updated code to ES5 w/ babel

--- src/game.js
+++ src/game.js
@@ -1,15 +1,15 @@
-/* INSTRUCTIONS
-  To play Minesweeper, we wil create instances of MineSweeperGame
-in command line.
-  For Example:
-    In the command line, navigate to the lib directory and rune `node`.
-    Run `.load game .js` to load the contents of this file.
-    Then create a Game instance and run commands like so:
-      let game - new game(3, 3, 3);
-      game.playMove(0, 1);
-      game.playMove(1, 2);
-      When done, run `.exit`;
-*/
+// INSTRUCTIONS
+//  To play Minesweeper, we wil create instances of MineSweeperGame
+//    in command line.
+//  For Example:
+//    In the command line, navigate to the lib directory and rune `node`.
+//    Run `.load game .js` to load the contents of this file.
+//    Then create a Game instance and run commands like so:
+//      let game - new game(3, 3, 3);
+//      game.playMove(0, 1);
+//      game.playMove(1, 2);
+//      When done, run `.exit`;
+
 
 import Board from './board.js';
 
